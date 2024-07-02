7da87c1e71be8c3e5abb0a2de2bc60b779f8a85a
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-07-02 17:57:39 +0530

    Rendering Arrays

--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,12 @@
 import Product from "./Product.jsx"
 
 function ProductTab() {
+    let options = ["Hi-Tech", "Fast", "Durable"];
     return (
     <>
-    <Product title="Phone" price={159999}/>
-    <Product title="Laptop"price={199999}/>
-    <Product title="Pen" price={20}/>
+    <Product title="Phone" price={159999} features={options}/>
+    <Product title="Laptop"price={199999} features={options}/>
+    <Product title="Pen" price={20} features={options}/>
     </>
     )
 }
