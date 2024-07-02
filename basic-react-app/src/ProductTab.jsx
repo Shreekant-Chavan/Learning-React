cffcdd217ed178681d2a2f66b75623af6e4382f7
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-07-02 17:16:40 +0530

    Props in React

--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product.jsx"
 function ProductTab() {
     return (
     <>
-    <Product/>
-    <Product/>
-    <Product/>
+    <Product title="Phone" price={159999}/>
+    <Product title="Laptop"price={199999}/>
+    <Product title="Pen" price={20}/>
     </>
     )
 }
